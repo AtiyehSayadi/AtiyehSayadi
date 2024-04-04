e8b98c153bd1c16c423aac7119f75cca650fc90b
AtiyehSayadi <156868459+AtiyehSayadi@users.noreply.github.com>
2024-04-03 23:16:49 -0400

    Unit Testing, The Final File

--- src/graph.py
+++ src/graph.py
@@ -14,17 +14,23 @@ class Graph:
     def exist_edge(self,start,end):
         """This function indicates whether the desired edge exists
         in the adjacency matrix or not."""
+        if start<0 or start>=len(self.graph) or end <0 or  end >= len(self.graph):
+            return False
         if self.graph[start][end]==1:
             return True
         else:
             return False
     def get_degree(self,node):
         """This function calculates the degree of each node."""
+        if node<0 or node>=len(self.graph):
+            return 0
         row = self.graph[node]
         degree = sum(row)
         return degree
     def get_shortest_path(self,start, des):
         """This function calculates the shortest path length between two nodes."""
+        if start<0 or start>=len(self.graph) or des <0 or  des >= len(self.graph):
+            return float('inf')
         n = len(self.graph)
         unvisited_nodes=list(range(n))
         min_distance=1
@@ -55,16 +61,23 @@ class Graph:
         return float('inf')
     def degree_centrality(self,node):
         """This function calculates the degree centrality."""
+        if node<0 or node>=len(self.graph):
+            return 0
         n = len(self.graph)
         dc= (self.get_degree(node))/(n-1)
         return dc
     def closeness_centrality(self,node):
         """This function calculates the closeness centrality."""
+        if node<0 or node>=len(self.graph):
+            return 0
         n = len(self.graph)
         sum_path=0
         i=0
         while i<n:
+            if self.get_shortest_path(node,i)== float('inf'):
+                return 0
             sum_path += self.get_shortest_path(node,i)
             i+=1
         cc=(n-1)/ sum_path
         return cc
+    
